4cfc66552977cf72d246a1a4272eb5f6958b9829
Zaman Ali <138426327+zamanali423@users.noreply.github.com>
2024-11-25 19:58:08 -0800

    Create RealTimeUpdates.js

---
+++ src/pages/RealTimeUpdates.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+
+const socket = io("ws://webscrappingbackend.vercel.app");
+
+function RealTimeUpdates() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    socket.on("batchUpdate", (batch) => {
+      setData((prevData) => [...prevData, ...batch]);
+    });
+
+    socket.on("scrapingComplete", (message) => {
+      console.log(message);
+    });
+
+    socket.on("scrapingError", (error) => {
+      console.error("Scraping error:", error.message);
+    });
+
+    return () => socket.disconnect();
+  }, []);
+
+  return (
+    <div>
+      <h1>Live Scraping Data</h1>
+      {data.map((item, index) => (
+        <div key={index}>
+          <h3>{item.storeName}</h3>
+          <p>{item.address}</p>
+          {/* Display other fields */}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default RealTimeUpdates;
